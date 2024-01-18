2dd268bbd04bbb8b4b93213a9caef66f842aae3c
cristianrubioa <cristianrubioa@gmail.com>
2024-01-18 15:33:01 -0500

    Update short_help and help in commands

--- src/commands.py
+++ src/commands.py
@@ -8,21 +8,32 @@ from src.handlers import process_satellite_images_metadata
 app = typer.Typer(help="Handler functions to execute.")
 
 
-@app.command(help="Extract image files data and organize metadata in json file.")
+@app.command(
+    short_help="Extract image files data and organize metadata in json file.",
+    help=process_satellite_images_metadata.__doc__,
+)
 def metadata():
     process_satellite_images_metadata()
 
 
-@app.command(help="Clips images based on shapefile geometries.")
+@app.command(
+    short_help="Clips images based on shapefile geometries.",
+    help=process_and_clip_landsat_images.__doc__,
+)
 def cropped():
     process_and_clip_landsat_images()
 
 
-@app.command(help="Create true color images.")
+@app.command(
+    short_help="Create true color images.",
+    help=process_bands_for_true_color_image.__doc__,
+)
 def color():
     process_bands_for_true_color_image()
 
 
-@app.command(help="Create binary images.")
+@app.command(
+    short_help="Create binary images.", help=process_bands_for_binary_image.__doc__
+)
 def binary():
     process_bands_for_binary_image()
