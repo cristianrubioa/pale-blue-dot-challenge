80b13bdb851f9783726f87ae9fbeb4cbb5ca891e
cristianrubioa <cristianrubioa@gmail.com>
2024-01-17 22:20:24 -0500

    :wrench: Update settings - dataset paths

--- src/settings.py
+++ src/settings.py
@@ -7,9 +7,19 @@ from pydantic_settings import BaseSettings
 
 
 class ConfigSettings(BaseModel):
-    BASE_PATH: ClassVar[Path] = Path(__file__).resolve().parent.parent / "dataset"
-    ORIGINAL_DATASET_PATH: Path = BASE_PATH / "original_dataset"
+    # Paths
+    BASE_PATH: ClassVar[Path] = Path(__file__).resolve().parent.parent
+    DATASET_PATH: Path = BASE_PATH / "dataset"
+    SHAPEFILE_PATH: Path = BASE_PATH / "shapefile"
+    ORIGINAL_DATASET_PATH: Path = DATASET_PATH / "original_dataset"
+    ROI_CROPPED_DATASET_PATH: Path = DATASET_PATH / "roi_cropped_dataset"
+    ROI_CROPPED_COLOR_DATASET_PATH: Path = DATASET_PATH / "roi_cropped_color_dataset"
+    ROI_CROPPED_BINARY_DATASET_PATH: Path = DATASET_PATH / "roi_cropped_binary_dataset"
+    # Files
+    SHAPEFILE_SHP_FILE: str = "Demo.shp"
     ORIGINAL_DATASET_METADATA_FILE: str = "landsat_images_data.json"
+    # Extra
+    ORIGINAL_DATASET_IMAGE_EXTENSION: str = "TIF"
 
 
 class Settings(BaseSettings):
